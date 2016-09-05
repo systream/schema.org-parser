5100228b13eafd00df4cd419d0b1cb36be92a039
Tihanyi Péter <peter.tihanyi@bigfish.hu>
2016-09-05 09:17:35 +0200

    Balasz's suggestions

--- src/schema.org-parser.js
+++ src/schema.org-parser.js
@@ -1,99 +1,100 @@
 function getMicroFormats(domElement) {
 
-    if (!domElement) {
-        domElement = document;
-    }
+	if (!domElement) {
+		domElement = document;
+	}
 
 	var itemScopes = domElement.querySelectorAll('[itemscope]'),
 		microFormats = [],
 		processedItemScopes = [],
-        processedItemProps = [];
+		processedItemProps = [];
 
-    if (!itemScopes.length) {
-        return microFormats;
-    }
+	if (!itemScopes.length) {
+		return microFormats;
+	}
 
-    function getItemPropValue(itemPropName, currentItemProp) {
-        var result;
-        if (itemPropName === 'url') {
-            result = currentItemProp
-                .getAttribute('href')
-                .replace(/\s+/g, ' ')
-                .trim();
-        } else if(itemPropName === 'image') {
-            result = currentItemProp
-                .getAttribute('src')
-                .replace(/\s+/g, ' ')
-                .trim();
-        } else {
+	function getItemPropValue(itemPropName, currentItemProp) {
+		var result;
+		if (itemPropName === 'url') {
+			result = currentItemProp
+				.getAttribute('href')
+				.replace(/\s+/g, ' ')
+				.trim();
+		} else if (itemPropName === 'image') {
+			result = currentItemProp
+				.getAttribute('src')
+				.replace(/\s+/g, ' ')
+				.trim();
+		} else {
 
-            if (currentItemProp.hasAttribute('content')) {
-                result = currentItemProp
-                    .getAttribute('content')
-                    .replace(/\s+/g, ' ')
-                    .trim();
-            } else {
-                result = currentItemProp
-                    .textContent
-                    .replace(/\s+/g, ' ')
-                    .trim();
-            }
-        }
-        return result;
-    }
+			if (currentItemProp.hasAttribute('content')) {
+				result = currentItemProp
+					.getAttribute('content')
+					.replace(/\s+/g, ' ')
+					.trim();
+			} else {
+				result = currentItemProp
+					.textContent
+					.replace(/\s+/g, ' ')
+					.trim();
+			}
+		}
+		return result;
+	}
 
-    /**
-     *
-     * @param currentItemScope
-     * @returns {{}}
-     */
-    function processItemScope(currentItemScope) {
+	/**
+	 *
+	 * @param currentItemScope
+	 * @returns {{}}
+	 */
+	function processItemScope(currentItemScope) {
 
-        var currentResultItem = {};
+		var currentResultItem = {};
 
-        currentResultItem['itemType'] = currentItemScope.getAttribute('itemtype').substr(18);
-        var itemProps = currentItemScope.querySelectorAll('[itemprop]');
+		currentResultItem['itemType'] = currentItemScope.getAttribute('itemtype').substr(18);
+		var itemProps = currentItemScope.querySelectorAll('[itemprop]');
 
-        processedItemScopes.push(currentItemScope);
+		processedItemScopes.push(currentItemScope);
+		var y;
+		for (y in itemProps) {
+			if (itemProps.hasOwnProperty(y)) {
 
-        for (var y in itemProps) {
-            if (itemProps.hasOwnProperty(y)) {
+				if (processedItemProps.indexOf(itemProps[y]) !== -1) {
+					continue;
+				}
 
-                if (processedItemProps.indexOf(itemProps[y]) !== -1) {
-                    continue;
-                }
+				var currentItemProp = itemProps[y],
+					itemPropName;
 
-                var currentItemProp = itemProps[y],
-                    itemPropName;
+				processedItemProps.push(currentItemProp);
 
-                processedItemProps.push(currentItemProp);
+				if (currentItemProp === undefined) {
+					continue;
+				}
 
-                if (currentItemProp === undefined) {
-                    continue;
-                }
+				itemPropName = currentItemProp.getAttribute('itemprop').replace(/\s+/g, ' ').trim();
 
-                itemPropName = currentItemProp.getAttribute('itemprop').replace(/\s+/g, ' ').trim();
+				if (currentItemProp.hasAttribute('itemscope')) {
+					currentResultItem[itemPropName] = processItemScope(currentItemProp);
+					continue;
+				}
+				currentResultItem[itemPropName] = getItemPropValue(itemPropName, currentItemProp);
+			}
+		}
+		return currentResultItem;
+	}
 
-                if (currentItemProp.hasAttribute('itemscope')) {
-                    currentResultItem[itemPropName] = processItemScope(currentItemProp);
-                    continue;
-                }
-                currentResultItem[itemPropName] = getItemPropValue(itemPropName, currentItemProp);
-            }
-        }
-        return currentResultItem;
-    }
-
-    // iterate throw item scopes
-	for (var x in itemScopes) {
+	// iterate throw item scopes
+	var x;
+	for (x in itemScopes) {
 		if (itemScopes.hasOwnProperty(x)) {
-            if (processedItemScopes.indexOf(itemScopes[x]) !== -1) {
-                continue;
-            }
+			if (processedItemScopes.indexOf(itemScopes[x]) !== -1) {
+				continue;
+			}
 
-            var currentResultItem = processItemScope(itemScopes[x]);
-            microFormats.push(currentResultItem);
-        }
+			var currentResultItem = processItemScope(itemScopes[x]);
+			microFormats.push(currentResultItem);
+		}
 	}
 	return microFormats;
 }
